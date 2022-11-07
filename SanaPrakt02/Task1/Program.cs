f25a33ce66722ae8868ea236e10753fa4394381c
Vadym Dukhopela <kb201_dvo@student.ztu.edu.ua>
2022-11-08 01:44:41 +0200

    changed first 3 lines

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp” та замінити останній комміт");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp” та замінити останній комміт");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp” та замінити останній комміт");
+Console.WriteLine("Lorem ipsum dolor sit amet,");
+Console.WriteLine("consectetur adipiscing elit");
+Console.WriteLine("sed do eiusmod tempor incididunt ut labore et dolore magna aliqua");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp” та замінити останній комміт");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp” та замінити останній комміт");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp” та замінити останній комміт");
